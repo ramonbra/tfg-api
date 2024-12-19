57d09172388b8f1ad3d6aee190e8432a5794ac05
ramonbra <r.monzo.b@gmail.com>
2024-12-19 22:53:02 +0100

    Removed unused import

--- index.ts
+++ index.ts
@@ -2,7 +2,6 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import professorRouter from "./src/routes/professor.routes.ts";
-import { hashPassword } from "./src/services/hasher.service.ts";
 
 dotenv.config();
 const app = express();
